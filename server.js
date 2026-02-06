9f5c9a8f9b65aa55f5add7ef71b6d810e5cb91a9
Obianuju-Sunday <obianujusunday43@gmail.com>
2026-02-06 19:09:57 +0100

    Add GET /todos endpoint

--- server.js
+++ server.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const app = express();
 
+
+// TODO: Create a route to get all todos
+var todos = [
+    {
+        id: 1,
+        title: 'Buy groceries',
+        isCompleted: false
+    },
+    {
+            id: 2,
+            title: 'Walk the dog',
+            isCompleted: true
+    },
+    {
+        id: 3,
+        title: 'Read a book',
+        isCompleted: false
+    },
+    {
+        id: 4,
+        title: 'Write code/ Do day 2 challenge of my backend glow up journey',
+        isCompleted: true
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Root route
 app.get('/', (req, res) => {
   res.json({ message: 'My backend journey starts now' });
 });
+
+// todos route
+app.get('/todos', (req,res) => {
+    res.json(todos);
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
