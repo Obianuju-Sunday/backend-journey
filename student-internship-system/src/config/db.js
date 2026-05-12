f7848a44de9c8268b68c00b5e8f975c18dfabccd
Obianuju-Sunday <obianujusunday43@gmail.com>
2026-05-12 19:11:26 +0100

    fix: postgres connection

--- student-internship-system/src/config/db.js
+++ student-internship-system/src/config/db.js
@@ -1,16 +1,9 @@
+require('dotenv').config();
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || undefined,
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
