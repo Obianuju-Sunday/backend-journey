388705a4f6f037621bf00b9f69b308b549d1e1aa
Obianuju-Sunday <obianujusunday43@gmail.com>
2026-08-28 13:25:05 +0100

    include skills in student profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,23 @@ const getStudentProfile = async (req, res) => {
       return res.status(404).json({ error: 'Profile not found' });
     }
 
-    res.status(200).json(profile.rows[0]);
+    const studentProfileId = profile.rows[0].id;
+
+    const skills = await pool.query(
+      `SELECT
+        s.skill_name,
+        ss.proficiency
+      FROM student_skills ss
+      JOIN skills s ON ss.skill_id = s.id
+      WHERE ss.student_id = $1
+      ORDER BY s.skill_name`,
+      [studentProfileId]
+    );
+
+    return res.status(200).json({
+      ...profile.rows[0],
+      skills: skills.rows
+    });
 
   } catch (err) {
     console.error('Error fetching student profile:', err);
@@ -53,22 +69,36 @@ const updateOrgProfile = async (req, res) => {
 }
 
 const getStudentProfilePublic = async (req, res) => {
-  // uses req.params
-  res.json({ message: 'Coming soon' })
 
+  try {
+    const studentId = req.params.id;
 
+    const studentProfile = await pool.query(
+      'SELECT id, full_name, program, university, portfolio_link FROM student_profiles WHERE id = $1',
+      [studentId]
+    );
 
-  // PUBLIC ROUTE - No token from a visitor who isn't logged in
-  // if (!req.user) {
-  //   const profile = await pool.query(
-  //     'SELECT id, full_name, program, university, portfolio_link FROM student_profiles WHERE id = $1',
-  //     [targetStudentId]
-  //   );
+    if (studentProfile.rows.length === 0) {
+      return res.status(404).json({
+        error: 'Profile not found'
+      })
+    }
+
+    res.status(200).json(studentProfile.rows[0])
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Server error' });
+  }
 }
 
 const getOrgProfilePublic = async (req, res) => {
-  // Also uses req.params
-  res.json({ message: 'Coming soon' })
+  try {
+    const orgId = req.params.id;
+
+    const orgProfile = await pool.query('SELECT ')
+  } catch (error) {
+
+  }
 }
 
 module.exports = {
